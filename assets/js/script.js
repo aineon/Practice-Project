9766faae0650e6a06f3f61b3316dc24f67023398
aineon <enia.oneill@gmail.com>
2020-06-25 14:50:19 +0000

    createBoard function added

---
+++ assets/js/script.js
@@ -0,0 +1,315 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+const cardEasyList = [
+    {
+        name: 'boat',
+        img: 'assets/images/bunagee.jpg'
+    },
+    {
+        name: 'boat',
+        img: 'assets/images/bunagee.jpg'
+    },
+    {
+        name: 'waterfall',
+        img: 'assets/images/waterfall.jpg'
+    },
+    {
+        name: 'waterfall',
+        img: 'assets/images/waterfall.jpg'
+    },
+    { 
+        name: 'beach',
+        img: 'assets/images/donegal_beach.jpg'
+    },
+    {
+        name: 'beach',
+        img: 'assets/images/donegal_beach.jpg'
+    },
+    {
+        name: 'island',
+        img: 'assets/images/skellig_michael.jpg'
+    },
+    {
+        name: 'island',
+        img: 'assets/images/skellig_michael.jpg'
+    },
+    {
+        name: 'lighthouse',
+        img: 'assets/images/lighthouse.jpg'
+    },
+    {
+        name: 'lighthouse',
+        img: 'assets/images/lighthouse.jpg'
+    },
+    {
+        name: 'mountains',
+        img: 'assets/images/mountains.jpg'
+    },
+    {
+        name: 'mountains',
+        img: 'assets/images/mountains.jpg'
+    },
+];
+
+const cardListMedium = [
+    {
+        name: 'boat',
+        img: 'assets/images/bunagee.jpg'
+    },
+    {
+        name: 'boat',
+        img: 'assets/images/bunagee.jpg'
+    },
+    {
+        name: 'waterfall',
+        img: 'assets/images/waterfall.jpg'
+    },
+    {
+        name: 'waterfall',
+        img: 'assets/images/waterfall.jpg'
+    },
+    { 
+        name: 'beach',
+        img: 'assets/images/donegal_beach.jpg'
+    },
+    {
+        name: 'beach',
+        img: 'assets/images/donegal_beach.jpg'
+    },
+    {
+        name: 'island',
+        img: 'assets/images/skellig_michael.jpg'
+    },
+    {
+        name: 'island',
+        img: 'assets/images/skellig_michael.jpg'
+    },
+    {
+        name: 'lighthouse',
+        img: 'assets/images/lighthouse.jpg'
+    },
+    {
+        name: 'lighthouse',
+        img: 'assets/images/lighthouse.jpg'
+    },
+    {
+        name: 'mountains',
+        img: 'assets/images/mountains.jpg'
+    },
+    {
+        name: 'mountains',
+        img: 'assets/images/mountains.jpg'
+    },
+    {
+        name: 'arranmore',
+        img: 'assets/images/arranmore.jpg'
+    },
+    {
+        name: 'arranmore',
+        img: 'assets/images/arranmore.jpg'
+    },
+    {
+        name: 'national park',
+        img: 'assets/images/national_park.jpg'
+    },
+    {
+        name: 'national park',
+        img: 'assets/images/national_park.jpg'
+    },
+    {
+        name: 'portnoo',
+        img: 'assets/images/portnoo.jpg'
+    },
+    {
+        name: 'portnoo',
+        img: 'assets/images/portnoo.jpg'
+    },
+    {
+        name: 'dolmen',
+        img: 'assets/images/dolmen.jpg'
+    },
+    {
+        name: 'dolmen',
+        img: 'assets/images/dolmen.jpg'
+    },
+    {
+        name: 'geokaun',
+        img: 'assets/images/geokaun_mountain.jpg'
+    },
+    {
+        name: 'geokaun',
+        img: 'assets/images/geokaun_mountain.jpg'
+    },
+    {
+        name: 'tay',
+        img: 'assets/images/lough_tay.jpg'
+    },
+    {
+        name: 'tay',
+        img: 'assets/images/lough_tay.jpg' 
+    }, 
+];
+
+ const cardListHard = [
+        {
+        name: 'boat',
+        img: 'assets/images/bunagee.jpg'
+    },
+    {
+        name: 'boat',
+        img: 'assets/images/bunagee.jpg'
+    },
+    {
+        name: 'waterfall',
+        img: 'assets/images/waterfall.jpg'
+    },
+    {
+        name: 'waterfall',
+        img: 'assets/images/waterfall.jpg'
+    },
+    { 
+        name: 'beach',
+        img: 'assets/images/donegal_beach.jpg'
+    },
+    {
+        name: 'beach',
+        img: 'assets/images/donegal_beach.jpg'
+    },
+    {
+        name: 'island',
+        img: 'assets/images/skellig_michael.jpg'
+    },
+    {
+        name: 'island',
+        img: 'assets/images/skellig_michael.jpg'
+    },
+    {
+        name: 'lighthouse',
+        img: 'assets/images/lighthouse.jpg'
+    },
+    {
+        name: 'lighthouse',
+        img: 'assets/images/lighthouse.jpg'
+    },
+    {
+        name: 'mountains',
+        img: 'assets/images/mountains.jpg'
+    },
+    {
+        name: 'mountains',
+        img: 'assets/images/mountains.jpg'
+    },
+    {
+        name: 'arranmore',
+        img: 'assets/images/arranmore.jpg'
+    },
+    {
+        name: 'arranmore',
+        img: 'assets/images/arranmore.jpg'
+    },
+    {
+        name: 'national park',
+        img: 'assets/images/national_park.jpg'
+    },
+    {
+        name: 'national park',
+        img: 'assets/images/national_park.jpg'
+    },
+    {
+        name: 'portnoo',
+        img: 'assets/images/portnoo.jpg'
+    },
+    {
+        name: 'portnoo',
+        img: 'assets/images/portnoo.jpg'
+    },
+    {
+        name: 'dolmen',
+        img: 'assets/images/dolmen.jpg'
+    },
+    {
+        name: 'dolmen',
+        img: 'assets/images/dolmen.jpg'
+    },
+    {
+        name: 'tay',
+        img: 'assets/images/lough_tay.jpg'
+    },
+    {
+        name: 'tay',
+        img: 'assets/images/lough_tay.jpg' 
+    },
+    {
+        name: 'forty',
+        img: 'assets/images/forty_foot.jpg' 
+    },
+    {
+        name: 'forty',
+        img: 'assets/images/forty_foot.jpg' 
+    },
+    {
+        name: 'breeches',
+        img: 'assets/images/the_breeches.jpg' 
+    },
+    {
+        name: 'breeches',
+        img: 'assets/images/the_breeches.jpg' 
+    },
+    {
+        name: 'gougane',
+        img: 'assets/images/gougane_barra.jpg' 
+    },
+    {
+        name: 'gougane',
+        img: 'assets/images/gougane_barra.jpg'
+    },
+    {
+        name: 'bective',
+        img: 'assets/images/bective.jpg'
+    },
+    {
+        name: 'bective',
+        img: 'assets/images/bective.jpg'
+    },
+    {
+        name: 'kerry',
+        img: 'assets/images/kerry.jpg'
+    },
+    {
+        name: 'kerry',
+        img: 'assets/images/kerry.jpg'
+    },
+    {
+        name: 'letterfrack',
+        img: 'assets/images/cliff.jpg'
+    },
+    {
+        name: 'letterfrack',
+        img: 'assets/images/cliff.jpg'
+    },
+    {
+        name: 'lake',
+        img: 'assets/images/lake.jpg'
+    },
+    {
+        name: 'lake',
+        img: 'assets/images/lake.jpg'
+    }
+ ];
+
+ const grid = document.querySelector('.grid')
+
+ function createBoardEasy() {
+     for (let i = 0; i < cardEasyList.length; i++) {
+         let cards = document.createElement('img')
+         cards.setAttribute('src', 'assets/images/ireland.jpg')
+         cards.setAttribute('data-id', i)
+         cards.classList.add('card')
+         //card.addEventListener('click', flipcard)
+         grid.appendChild(cards)  
+        }
+ }
+createBoardEasy();
+ })
